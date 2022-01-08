51cc801ac3c9bf9cdcd95468e3985948a7f51be8
aogara-ds <78378219+aogara-ds@users.noreply.github.com>
2022-01-08 11:48:00 -0800

    Delete config.py

--- work_trial/config.py
+++
@@ -1,9 +0,0 @@
-"""
-File to securely store private information outside of GitHub.  
-
-"""
-
-# Personal AWS key
-ACCESS_KEY = "AKIAUUXJYG33APU5G5BU"
-SECRET_KEY = "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"
-
